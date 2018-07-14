fe8d47350706bad3f62d8bc4e426b85c915e56dc
pratheept <pratheept@hotmail.com>
2018-07-14 20:20:34 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@
 
 $access_token = 'djrm3KX3XIY7wQv9dcr8kDI288emZ9szQb0j3GVjD9kia7p5w0c3liomcXiuoVlQDKlK/2cElR91wMR+zUSINxLLU6FEst4k+GqEDsAevoXWNx9yW7uTXqF/A6aVzlkA1gBh7HaCIt4yRY3DKbzT2AdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'Uc46ce37f2264f46a4e2fa4f9054dd19e';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
